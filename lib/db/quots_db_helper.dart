e6d037364870c731e40f8fa107b64ce574834ca8
Jeel Bhatti <jeelbhatti9@gmail.com>
2022-01-28 15:01:57 +0530

    changes done

--- lib/db/quots_db_helper.dart
+++ lib/db/quots_db_helper.dart
@@ -5,49 +5,42 @@ import 'dart:typed_data';
 import 'package:flutter/services.dart';
 import 'package:sqflite/sqflite.dart';
 
-class DBHelper{
-
+class DBHelper {
   late Database db;
-  List  category  = [];
-  List  quotes  = [];
+  List category = [];
+  List quotes = [];
   //
-  Future databaseGet() async{
+  Future databaseGet() async {
+    var databasePath = await getDatabasesPath();
 
-    var databasePath  =  await  getDatabasesPath();
-
-    var path =  join(databasePath, "quotes.db");
+    var path = join(databasePath, "quotes.db");
     print(path);
     // Check if the database exists
     var exists = await databaseExists(path);
     print(exists);
-    if(!exists){
+    if (!exists) {
       // Should happen only the first time you launch your application
       // Make sure the parent directory exists
-      try{
+      try {
         await Directory(dirname(path)).create(recursive: true);
-
-      }catch(e){
+      } catch (e) {
         ByteData data = await rootBundle.load(join("assets", "quotes.db"));
         List<int> bytes =
-        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+            data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
         // Write and flush the bytes written
         await File(path).writeAsBytes(bytes, flush: true);
       }
-    }else {
+    } else {
       var db = await openDatabase(path);
-      print("here");
-      category =  await   db.query('quotes');
-      print(category.length);
+
+      category = await db.query('category');
+      print(category);
     }
-
-
   }
 //
 // Future databaseGet() async{
 //   var databasesPath = await getDatabasesPath();
 //   String path = join(databasesPath,'assets/quotes.db');
 // }
-
-
 
 }
